8992bd73f3a53ff34a9522ffd2b727f7a6ed1a8f
Arina Natasha <60564848+ArinaNatasha@users.noreply.github.com>
2024-01-09 20:20:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ st.set_page_config(layout="wide")
 tab1, tab2, tab3, tab4 = st.tabs(["Home", "Exploratory Data Analysis", "Student List", "Engagement Predictor"])
 
 # Import Dataset
-df = pd.read_csv("cleanedData.csv")
-engagement_df = pd.read_csv("dataEngagement.csv")
+df = pd.read_csv("https://drive.google.com/file/d/1yGMRwOEf1ZN1dtQg9manb5CalN9MspIX/view?usp=sharing")
+engagement_df = pd.read_csv("https://drive.google.com/file/d/1t9RaTi6hZINN_DD1pd2qjNwog07aboVz/view?usp=drive_link")
 
 with tab1:
     st.title("WIH3001 DATA SCIENCE PROJECT")
